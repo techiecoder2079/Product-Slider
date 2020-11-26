1aeff1654646264df9430b5657f10160105ddd1b
Techie Coder <71074789+techiecoder2079@users.noreply.github.com>
2020-11-26 15:32:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
